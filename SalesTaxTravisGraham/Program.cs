73b925a8a86ea1d78290d83ca3b224f27cbf9fcc
Travis Graham <travis.graham@legrand.us>
2021-03-27 10:40:36 -0600

    Initial

---
+++ SalesTaxTravisGraham/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesTaxTravisGraham
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
