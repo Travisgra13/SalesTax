7bfd4f2fa25f8d807e9afebcdc2cecaaedf08166
Travis Graham <travis.graham@legrand.us>
2021-03-28 10:43:26 -0600

    Added Second Test Case

--- SalesTaxUnitTests/SalesTaxUnitTests/UnitTestsSalesTax.cs
+++ SalesTaxUnitTests/SalesTaxUnitTests/UnitTestsSalesTax.cs
@@ -18,7 +18,7 @@ namespace SalesTaxUnitTests
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestCase1()
         {
             Item.Item book1 = new Item.NonTaxableItem("Book", 12.49, 1, false);
             Item.Item book2 = new Item.NonTaxableItem("Book", 12.49, 1, false);
@@ -47,9 +47,35 @@ namespace SalesTaxUnitTests
                                 "Total: 42.32\n";
             Tuple<String, String> result = normalizeExpectedActual(expected, outputter.ToString());
             Assert.IsTrue(String.Equals(result.Item1, result.Item2, StringComparison.OrdinalIgnoreCase));
+        }
 
+        [TestMethod]
+        public void TestCase1UsingQuantityGreaterThan1()
+        {
+            Item.Item book1 = new Item.NonTaxableItem("Book", 12.49, 2, false);
+            Item.Item music = new Item.TaxableItem("Music CD", 14.99, 1, false);
+            Item.Item chocolate = new Item.NonTaxableItem("Chocolate bar", 0.85, 1, false);
 
+            Outputter outputter = new Outputter();
+            List<Item.Item> bookList = new List<Item.Item>();
+            bookList.Add(book1);
+            List<Item.Item> musicList = new List<Item.Item>();
+            musicList.Add(music);
 
+            List<Item.Item> chocolateList = new List<Item.Item>();
+            chocolateList.Add(chocolate);
+
+            outputter.Add(book1.GetItemName(), bookList);
+            outputter.Add(music.GetItemName(), musicList);
+            outputter.Add(chocolate.GetItemName(), chocolateList);
+
+            string expected = "Book: 24.98 (2 @ 12.49)\n" +
+                                "Music CD: 16.49\n" +
+                                "Chocolate bar: 0.85\n" +
+                                "Sales Taxes: 1.50\n" +
+                                "Total: 42.32\n";
+            Tuple<String, String> result = normalizeExpectedActual(expected, outputter.ToString());
+            Assert.IsTrue(String.Equals(result.Item1, result.Item2, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
